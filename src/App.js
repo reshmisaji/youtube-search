78265dc0db071f275060146dbd8979f362d61c6a
Reshmi <reshmisaji50@gmail.com>
2023-07-20 00:39:43 +0530

    Create basic structure for header and search bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { SearchBar } from './components/SearchBar';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          You can start your app here
-        </p>
-        <p>
-          Or get hypnotized by this nice circly thingy
-        </p>
+    <div className="app">
+      <header data-testid="app-header" className="app-header">
+        Youtube Search
       </header>
+      <SearchBar />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('Should render header text properly', () => {
+    render(<App />);
+    const header = screen.getByTestId("app-header");
+    expect(header).toBeInTheDocument();
+    expect(header.textContent).toBe("Youtube Search")
+  });
+
+  it('Should render search bar', () => {
+    render(<App />);
+    const searchBar = screen.getByTestId("search-bar");
+    expect(searchBar).toBeInTheDocument();
+  });
+})

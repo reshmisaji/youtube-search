17c444de8cce66224d81c3f0f4e458375269ff4a
Reshmi <reshmisaji50@gmail.com>
2023-07-21 15:02:24 +0530

    Add infinite scroll feature

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 import React from 'react';
-import { search } from './apis/YouTube';
+import * as YouTube from './clients/YouTube';
+const { search } = YouTube;
 
 const mockSearchBarComponent = jest.fn()
+const mockSearchResultsComponent = jest.fn()
 
 jest.mock("./components/SearchBar", () => (props) => {
   mockSearchBarComponent(props)
@@ -14,15 +16,24 @@ jest.mock("./components/SearchBar", () => (props) => {
   </>);
 });
 
-jest.mock('./apis/YouTube', () => ({
+jest.mock("./components/SearchResults", () => (props) => {
+  mockSearchResultsComponent(props)
+  const { results } = props
+  return (<div data-testid="search-results" id="search-results">
+    {results && results.map((result, index) => (<div key={index}>{result?.snippet?.title}</div>))}
+  </div>);
+});
+
+jest.mock('./clients/YouTube', () => ({
   search: jest.fn()
 }))
+
+
 
 describe('App', () => {
 
   it('Should render header text properly', () => {
     render(<App />);
-    
     const header = screen.getByTestId("app-header");
 
     expect(header).toBeInTheDocument();
@@ -45,11 +56,39 @@ describe('App', () => {
     }))
   })
 
-  it('Should invoke search api when handleOnSearch is triggered', () => {
+  it('Should invoke search api when handleOnSearch is triggered', async () => {
     render(<App />);
-    fireEvent.click(screen.getByTestId("search-bar"))
-    fireEvent.click(screen.getByTestId("search-icon"))
+    search.mockResolvedValue()
 
-    expect(search).toHaveBeenCalledWith("new value");
+    await waitFor(() => {
+      fireEvent.click(screen.getByTestId("search-bar"))
+      fireEvent.click(screen.getByTestId("search-icon"))
+
+      expect(search).toHaveBeenCalled();
+    });
+  });
+
+  it('Should not render search results container when results are empty or not present', async () => {
+    render(<App />);
+    search.mockResolvedValue()
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByTestId("search-icon"))
+      const searchResult = screen.queryByTestId("search-results");
+
+      expect(searchResult).not.toBeInTheDocument();
+    });
+  });
+
+  it('Should render search results container when results are present', async () => {
+    render(<App />);
+    search.mockResolvedValue({ data: { items: [{ snippet: { title: "flower" } }], nextPageToken: "123" } })
+
+    await waitFor(() => {
+      fireEvent.click(screen.getByTestId("search-icon"))
+      const searchResult = screen.getByTestId("search-results");
+
+      expect(searchResult).toBeInTheDocument();
+    });
   });
 })
--- src/clients/YouTube.js
+++ src/clients/YouTube.js
@@ -1,14 +1,17 @@
 import axios from "axios"
 
-export const search = (searchValue) => {
-    return axios({
+export const search = (searchValue, nextPageToken) => {
+    const request = {
         "method": "GET",
         "url": 'https://www.googleapis.com/youtube/v3/search',
         "params": {
             'part': 'snippet',
             'maxResults': '20',
             'key': process.env.REACT_APP_YOUTUBE_API_KEY,
-            'q': searchValue
+            'q': searchValue,
         }
-    })
+    }
+    if (nextPageToken) request.params.pageToken = nextPageToken
+
+    return axios(request)
 }
